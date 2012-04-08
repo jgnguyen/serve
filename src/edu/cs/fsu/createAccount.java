b3ac3521880ed24fe4b9db54182b9f889673e1c6
John Nguyen <nguyen.john@me.com>
2012-04-08 15:52:42 -0400

    Account creation added

--- src/edu/cs/fsu/createAccount.java
+++ src/edu/cs/fsu/createAccount.java
@@ -1,10 +1,17 @@
 package edu.cs.fsu;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.http.client.ClientProtocolException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class createAccount extends Activity{
 
@@ -18,20 +25,22 @@ public class createAccount extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createaccount);
 	}
-	public void submitNewAccountClick(View v)
+	public void submitNewAccountClick(View v) throws NoSuchAlgorithmException, ClientProtocolException, IOException
 	{
     	username = (EditText) findViewById(R.id.et_username);
     	password = (EditText) findViewById(R.id.et_password);
     	firstname = (EditText) findViewById(R.id.et_password);
     	lastname = (EditText) findViewById(R.id.et_password);
     	
-    	// check the DB to see if the user already exist
-    	// insert him into the database with all his info
-    	
-    	
-    	
-    	// issue intent to the session picker.
-    	Intent i = new Intent(this,edu.cs.fsu.sessionPicker.class);
-    	startActivity(i);
+		String url = String.format("http://www.fsurugby.org/serve/request.php?new_user=1&username=%s&password=%s&fname=&s&lname=%s", username.getText().toString(), serveUtilities.SHA1(password.getText().toString()), firstname.getText().toString(), lastname.getText().toString());
+		String result = serveUtilities.getJsonFromUrl(url);
+		
+		if (result.equals("good")) {
+	    	// issue intent to the session picker.
+	    	Intent i = new Intent(this,edu.cs.fsu.sessionPicker.class);
+	    	startActivity(i);
+		} else {
+			Toast.makeText(this, "Could not create account", Toast.LENGTH_LONG).show();
+		}
 	}
 }
